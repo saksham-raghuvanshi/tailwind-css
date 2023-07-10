734653dd02c891cc0a561f8716293a01f0499d13
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-07-10 19:09:05 +0530

    Header,Body and Footer

---
+++ script.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  darkMode: "class",
+
+  theme: {
+    extend: {
+      colors: {
+        logo: {
+          700: "#1C59AE",
+          800: "#114a9a",
+        },
+      },
+    },
+  },
+};
